e0a8521f7c32122b34735ffbfa0992ee2c02ae92
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-02 22:26:45 +0530

    add run file

--- invalidate run.bat
+++ invalidate run.bat
@@ -4,3 +4,11 @@ REM Creation Date : 2017-01-02
 REM Author : Lal Thomas 	
 REM Original File : D:\Dropbox\project\20151116-invalidate todotxt cli addon\invalidate 	
 	
+%~d0
+cd "%~p0"
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+pushd "%userProfile%\.todo.actions.d"
+echo "%scriptFolderPath%"
+mklink /d invalidate "%scriptFolderPath%"
+popd
