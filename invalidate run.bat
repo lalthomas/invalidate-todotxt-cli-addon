35d72a54ff346fd7cbc25eb0d006c53894b917cd
Îalthomas <lal.thomas.mail@gmail.com>
2020-08-01 16:49:39 +0700

    Update invalidate run.bat

--- invalidate run.bat
+++ invalidate run.bat
@@ -1,8 +1,7 @@
 @echo OFF 	
 REM File : invalidate run.bat 	
 REM Creation Date : 2017-01-02 	
-REM Author : Lal Thomas 	
-REM Original File : D:\Dropbox\project\20151116-invalidate todotxt cli addon\invalidate 	
+REM Author : Lal Thomas
 	
 %~d0
 cd "%~p0"
